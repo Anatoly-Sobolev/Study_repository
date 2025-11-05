3f06d83518e04414461cc822841511a74e3b8141
Toly-Leto <anatolii_stankovskiii@mail.ru>
2025-11-05 12:14:36 +0600

    commit

---
+++ Algorithms/Binary_Tree/Binary_Tree/Binary_Tree.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    int value;
+    Node* left;
+    Node* right;
+};
+
+Node* buildPerfectlyBalancedTree(int num_nodes, int level) {
+    if (num_nodes == 0) {
+        return nullptr;
+    }
+
+
+    Node* newNode = new Node;
+    newNode->value = level;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+
+
+    int left_subtree_nodes = (num_nodes - 1) / 2;
+    int right_subtree_nodes = (num_nodes - 1) - left_subtree_nodes;
+
+
+    newNode->left = buildPerfectlyBalancedTree(left_subtree_nodes, level + 1);
+
+    newNode->right = buildPerfectlyBalancedTree(right_subtree_nodes, level + 1);
+
+    return newNode;
+}
+
+
+void swapChildren(Node* node) {
+    if (node == nullptr) {
+        return;
+    }
+
+
+    if (node->left != nullptr || node->right != nullptr) {
+        Node* temp = node->left;
+        node->left = node->right;
+        node->right = temp;
+    }
+
+
+    swapChildren(node->left);
+    swapChildren(node->right);
+}
+
+
+void printTree(Node* node, int depth) {
+    if (node == nullptr) {
+        return;
+    }
+
+
+    for (int i = 0; i < depth; ++i) {
+        cout << "     _";
+    }
+    cout << node->value << endl;
+
+
+    printTree(node->left, depth + 1);
+    printTree(node->right, depth + 1);
+}
+
+
+void freeTree(Node* node) {
+    if (node == nullptr) {
+        return;
+    }
+    freeTree(node->left);
+    freeTree(node->right);
+
+    delete node;
+}
+
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    int N;
+    cout << "Введите количество вершин N для дерева: ";
+    cin >> N;
+
+    if (N < 1) {
+        cout << "Количество вершин должно быть не меньше 1." << endl;
+        return 1;
+    }
+
+
+    Node* root = buildPerfectlyBalancedTree(N, 10);
+
+    cout << "\nДерево до обмена дочерних вершин:" << endl;
+    printTree(root, 0);
+
+    swapChildren(root);
+
+    cout << "\nДерево после обмена дочерних вершин:" << endl;
+    printTree(root, 0);
+
+
+    freeTree(root);
+
+    return 0;
+}
---
+++ Algorithms/Linked_list/Linked_list/Linked_list.cpp
@@ -0,0 +1,173 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    string word;
+    int pages[10];
+    Node* next;
+};
+
+Node* addNode(Node* head, string newWord, int newPages[]) {
+    Node* newNode = new Node;
+    newNode->word = newWord;
+    for (int i = 0; i < 10; ++i) {
+        newNode->pages[i] = newPages[i];
+    }
+    newNode->next = nullptr;
+
+    if (head == nullptr) {
+        return newNode;
+    }
+    else {
+        Node* current = head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = newNode;
+        return head;
+    }
+}
+
+void displayList(Node* head) {
+    if (head == nullptr) {
+        cout << "The list is empty." << endl << endl << endl;
+        return;
+    }
+    Node* current = head;
+    while (current != nullptr) {
+        cout << "Слово: " << current->word << endl;
+        cout << "Страницы: [";
+        for (int i = 0; i < 10; ++i) {
+            cout << current->pages[i];
+            if (i < 9) {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+        cout << "---------------" << endl;
+        current = current->next;
+    }
+}
+
+void displayPagesByWord(Node* head, string searchWord) {
+    if (head == nullptr) {
+        cout << "Список пуст, слово '" << searchWord << "' не найдено." << endl;
+        return;
+    }
+    Node* current = head;
+    bool found = false;
+    while (current != nullptr) {
+        if (current->word == searchWord) {
+            cout << "Страницы для слова '" << searchWord << "': [";
+            for (int i = 0; i < 10; ++i) {
+                cout << current->pages[i];
+                if (i < 9) {
+                    cout << ", ";
+                }
+            }
+            cout << "]" << endl;
+            found = true;
+            break;
+        }
+        current = current->next;
+    }
+    if (!found) {
+        cout << "Слово '" << searchWord << "' не найдено в списке." << endl;
+    }
+}
+
+void deleteList(Node* head) {
+    Node* current = head;
+    Node* nextNode;
+    while (current != nullptr) {
+        nextNode = current->next;
+        delete current;
+        current = nextNode;
+    }
+}
+
+Node* make_first_nodes(void) {
+
+    setlocale(LC_ALL, "rus");
+
+    Node* head = nullptr;
+
+    int pages1[] = { 1, 5, 10, 15, 20, 25, 30, 35, 40, 45 };
+    head = addNode(head, "hello", pages1);
+
+    int pages2[] = { 2, 12, 22, 32, 42, 52, 62, 72, 82, 92 };
+    head = addNode(head, "cat", pages2);
+
+    int pages3[] = { 3, 13, 23, 33, 43, 53, 63, 73, 83, 93 };
+    head = addNode(head, "list", pages3);
+
+    int pages4[] = { 3, 13, 23, 33, 43, 53, 63, 73, 83, 93 };
+    head = addNode(head, "C++", pages4);
+
+
+    return head;
+
+
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    string word; //For input
+    int pages[10];
+    Node* head = make_first_nodes();
+
+
+
+
+    int input;
+    input = 10;
+    while (input != 0) {
+        cout << "Input number please" << endl << endl;
+        cout << "0 - Go out of the program" << endl;
+        cout << "1 - Add a note to the link list" << endl;
+        cout << "2 - Display hole link list" << endl;
+        cout << "3 - Find a word in the link list" << endl;
+        cout << "4 - Delite the link list" << endl;
+
+        cin >> input;
+        switch (input) {
+        case 0: cout << "The program is finished" << endl; break;
+
+        case 1: cout << "Please input a word: " << endl;
+            cin >> word;
+            cout << "Please input 10 pages: " << endl;
+            for (int i = 0; i < 10; ++i) {
+                cin >> pages[i];
+            }
+            head = addNode(head, word, pages);
+            cout << "word: \"" << word << "\" is added " << endl << endl;
+            continue;
+
+        case 2: displayList(head);  continue;
+
+
+        case 3: cout << "Please input a word: ";
+            cin >> word;
+            displayPagesByWord(head, word);
+            continue;
+
+
+        case 4: deleteList(head);
+            head = nullptr;
+
+            cout << "The list was delited" << endl;
+            cout << "______________________" << endl << endl;
+            continue;
+
+        default: continue;
+        }
+
+
+
+    }
+
+    return 0;
+}
